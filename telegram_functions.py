3ecd6c24d17f4fe2612e40f249499af152e00aa8
aleksandrdiptan <diptan@huntflow.ru>
2022-10-15 10:30:58 +0300

    publish photo to Telegram channel.

--- telegram_functions.py
+++ telegram_functions.py
@@ -4,8 +4,11 @@ import telegram
 from dotenv import load_dotenv
 
 
-def send_message(tg_token: str, tg_chanel_id: str, message: str) -> None:
-    bot = telegram.Bot(token=tg_token)
+def upload_document(tg_chanel_id: str, document_path: str):
+    bot.send_document(chat_id=tg_chanel_id, document=open(document_path, 'rb'))
+
+
+def send_message(tg_chanel_id: str, message: str) -> None:
     bot.send_message(chat_id=tg_chanel_id, text=message)
 
 
@@ -13,5 +16,7 @@ if __name__ == '__main__':
     load_dotenv()
     telegram_token = os.environ['TELEGRAM_TOKEN']
     telegram_chanel_id = os.environ['TELEGRAM_CHANEL_ID']
+    bot = telegram.Bot(token=telegram_token)
 
-    send_message(telegram_token, telegram_chanel_id, 'Test message here.')
+    upload_document(telegram_chanel_id, 'images/gliese876_dss_1.gif')
+    # send_message(telegram_token, telegram_chanel_id, 'Test message here.')
