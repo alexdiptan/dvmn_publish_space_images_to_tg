1da22368355bb1e76f31bdf56faaee40354b41c1
aleksandrdiptan <diptan@huntflow.ru>
2022-10-15 13:28:44 +0300

    features added: - log messages about script work process - image_path script argument. When it filled script publish just one image

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+import logging
 import os
 import time
 from pathlib import Path
@@ -26,23 +28,28 @@ def get_images_from_folder(images_path) -> list:
 def publish_images(bot_instance, images: list, sleep_timer: int) -> None:
     for image in images:
         telegram_functions.upload_document(bot_instance, telegram_chanel_id, image)
+        logging.info(f'Publishing images {image} done. Next image will published after {sleep_timer} hours. Sleep.')
         time.sleep(sleep_timer * 3600)
 
 
 def main():
     Path(image_folder).mkdir(parents=True, exist_ok=True)
     image_files = get_images_from_folder(image_folder)
-    bot = telegram.Bot(token=telegram_token)
 
     while True:
         if len(image_files) == 0:
+            logging.info(f'Fetching images from APOD API')
             fetch_apod_images.fetch_apod_images(image_folder, apod_token)
+            logging.info(f'Fetching images from EPIC API')
             fetch_epic_images.fetch_epic_images('https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY')
+            logging.info(f'Fetching images from SpaceX API')
             fetch_spacex_images.fetch_spacex_launch_images(image_folder, '5eb87d47ffd86e000604b38a')
+            logging.info(f'Fetching images done')
         else:
-            publish_images(bot, image_files, int(publication_frequency))
+            logging.info(f'Start publishing images')
             shuffle(image_files)
             publish_images(bot, image_files, int(publication_frequency))
+            logging.info(f'Publishing images done')
 
 
 if __name__ == '__main__':
@@ -52,5 +59,15 @@ if __name__ == '__main__':
     telegram_token = os.environ['TELEGRAM_TOKEN']
     telegram_chanel_id = os.environ['TELEGRAM_CHANEL_ID']
     publication_frequency = os.environ['PUBLICATION_FREQUENCY']
+    bot = telegram.Bot(token=telegram_token)
 
-    main()
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image_path", help="Path to image which should be published to telegram.")
+    args = parser.parse_args()
+
+    if args.image_path:
+        telegram_functions.upload_document(bot, telegram_chanel_id, args.image_path)
+    else:
+        main()
